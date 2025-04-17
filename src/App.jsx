0c6d3ca4bf42397f431240950f818957c358906e
Justt-Mel <justinmelendezj@gmail.com>
2025-04-17 00:33:12 -0400

    finished map section of assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import {puppyList} from './data.js'
+import { useState } from 'react'
+
 function App() {
 
+  const [puppies, setPuppies] = useState(puppyList)
+//console.log(puppies)
+console.log("puppyList:", puppyList)
   return (
-   <div>
-    <h1>
-      hello world
-    </h1>
+   <div className= "App">
+    {
+        puppies.map((puppy) => {
+          return <p key = {puppy.id}>{puppy.name}</p>
+        })
+    }
    </div>
   )
 }
