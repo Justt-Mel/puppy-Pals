a945d33c25fb50a04ce937668e7a8989b506064f
Justt-Mel <justinmelendezj@gmail.com>
2025-04-17 01:26:16 -0400

    finished select featured puppy and added the css path to the program

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 import {puppyList} from './data.js'
 import { useState } from 'react'
+import './App.css'
 
 function App() {
 
-  const [puppies, setPuppies] = useState(puppyList)
-//console.log(puppies)
-console.log("puppyList:", puppyList)
+  const [puppies] = useState(puppyList)
+  const [featPupId, setfeatPupId] = useState(null)
+
+  console.log("puppyList:", puppyList)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+
   return (
    <div className= "App">
     {
-        puppies.map((puppy) => {
-          return <p key = {puppy.id}>{puppy.name}</p>
-        })
+      puppies.map((puppy) => {
+        console.log(featuredPup)
+        return <p onClick={() => {setfeatPupId(puppy.id)}} key= {puppy.id}>{puppy.name}</p>
+      })
     }
+    <p>{featPupId}</p>
+    {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
    </div>
   )
 }
